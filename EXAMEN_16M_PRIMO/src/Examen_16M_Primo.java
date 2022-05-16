e2f1a06bcc20e0bc4d2ff7f36dabb3ea5df27f23
costa <costa@IsmaPortatil>
2022-05-16 09:18:43 +0200

    Examen de Entornos 16M Ismael Costas

---
+++ EXAMEN_16M_PRIMO/src/Examen_16M_Primo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Examen_16M_Primo {
+
+	public static void main(String[] args) {
+		Scanner ent = new Scanner (System.in);
+		int num;
+		
+		
+		System.out.println("Buenos días Ceinmark, soy Ismael Costas");
+		
+		System.out.println("Introduce un número para averiguar si es primo o no: ");
+		num = ent.nextInt();
+		
+		
+
+	}
+
+}
